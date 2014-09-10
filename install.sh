1118c1640edf730425fb3082ccbe1727485f3760
Tomas Brambora <tomas.brambora@gmail.com>
2014-09-10 17:38:26 +0800

    allow-external and unverified for RBTools

--- install.sh
+++ install.sh
@@ -50,7 +50,8 @@ case "$1" in
       git clone "$REPO_HOME" "$REPO_NAME"
     fi
     echo "Installing python requirements"
-    pip install -r "$REPO_NAME/requirements.txt"
+    pip install -r "$REPO_NAME/requirements.txt" \
+      --allow-external RBTools --allow-unverified RBTools
     if [[ "$?" -ne "0" ]]; then
       exit 1
     fi
